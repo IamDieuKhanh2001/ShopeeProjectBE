0a3a459037c806898ed8e68004b4d72e96a34fa5
Mai Thanh Nhã <nhamt0306@gmail.com>
2022-07-18 09:35:05 +0700

    Config App

---
+++ FShop/src/main/java/com/example/fsoft_shopee_nhom02/FSoftShopeeNhom02Application.java
@@ -0,0 +1,14 @@
+package com.example.fsoft_shopee_nhom02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class FSoftShopeeNhom02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FSoftShopeeNhom02Application.class, args);
+    }
+
+}
---
+++ FShop/src/test/java/com/example/fsoft_shopee_nhom02/FSoftShopeeNhom02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fsoft_shopee_nhom02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FSoftShopeeNhom02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
