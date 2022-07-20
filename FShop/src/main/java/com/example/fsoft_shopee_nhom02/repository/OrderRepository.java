136bab49c2c03d58d072c5f61344b234576dc04f
VoHuuGiau2901 <vohuugiau2901@gmail.com>
2022-07-20 09:10:47 +0700

    Add base file for Order

--- FShop/src/main/java/com/example/fsoft_shopee_nhom02/repository/OrderRepository.java
+++ FShop/src/main/java/com/example/fsoft_shopee_nhom02/repository/OrderRepository.java
@@ -2,6 +2,8 @@ package com.example.fsoft_shopee_nhom02.repository;
 
 import com.example.fsoft_shopee_nhom02.model.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends JpaRepository<OrderEntity,Long> {
 }
--- FShop/src/main/java/com/example/fsoft_shopee_nhom02/service/impl/OrderDetailServiceImpl.java
+++ FShop/src/main/java/com/example/fsoft_shopee_nhom02/service/impl/OrderDetailServiceImpl.java
@@ -1,4 +1,25 @@
 package com.example.fsoft_shopee_nhom02.service.impl;
 
-public class OrderDetailServiceImpl {
+import com.example.fsoft_shopee_nhom02.model.OrderDetailsEntity;
+import com.example.fsoft_shopee_nhom02.repository.OrderDetailRepository;
+import com.example.fsoft_shopee_nhom02.service.OrderDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderDetailServiceImpl implements OrderDetailService {
+
+    private final OrderDetailRepository orderDetailRepository;
+
+    @Autowired
+    public OrderDetailServiceImpl(OrderDetailRepository orderDetailRepository) {
+        this.orderDetailRepository = orderDetailRepository;
+    }
+
+    @Override
+    public List<OrderDetailsEntity> getAll() {
+        return orderDetailRepository.findAll();
+    }
 }
