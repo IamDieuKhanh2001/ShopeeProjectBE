af85c3a316330afcf43093ce92c99ef4524f851a
griler <19110238@student.hcmute.edu.vn>
2022-07-29 15:00:09 +0700

    CartProductRespository

--- FShop/src/main/java/com/example/fsoft_shopee_nhom02/repository/CartProductRepository.java
+++ FShop/src/main/java/com/example/fsoft_shopee_nhom02/repository/CartProductRepository.java
@@ -1,6 +1,8 @@
 package com.example.fsoft_shopee_nhom02.repository;
 
+import com.example.fsoft_shopee_nhom02.dto.CartDetailDTO;
 import com.example.fsoft_shopee_nhom02.model.CartProductEntity;
+import org.aspectj.weaver.ast.Var;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -8,6 +10,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,4 +24,12 @@ public interface CartProductRepository extends JpaRepository<CartProductEntity, 
     @Transactional
     @Query("DELETE  FROM CartProductEntity addCart WHERE addCart.cartEntity.id=:cart_id and addCart.productEntity.id = :product_id")
     void deleteProduct(@Param("product_id")Long product_id,@Param("cart_id")Long cart_id);
+
+
+   @Query(" select product.name , type.type  , type.price,cart_product.quantity  ,product.image1 " +
+            "from CartProductEntity cart_product, TypeEntity  type , ProductEntity  product" +
+            " where product.id =cart_product.id and type.type = cart_product.type and cart_product.cartEntity.id =:cart_id")
+   List<HashMap<Object,Object>> getCartDetail(@Param("cart_id")Long cart_id);
+    @Query(" select c from CartProductEntity c where c.cartEntity.id =:cart_id")
+    List<CartProductEntity> getCart(@Param("cart_id")Long cart_id);
 }
