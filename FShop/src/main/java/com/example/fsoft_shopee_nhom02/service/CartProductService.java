4a835ae3a49d4f082826903cb904b3836bb684e4
griler <19110238@student.hcmute.edu.vn>
2022-07-29 14:58:41 +0700

    CartProductService

--- FShop/src/main/java/com/example/fsoft_shopee_nhom02/service/CartProductService.java
+++ FShop/src/main/java/com/example/fsoft_shopee_nhom02/service/CartProductService.java
@@ -1,26 +1,38 @@
 package com.example.fsoft_shopee_nhom02.service;
 
+import com.example.fsoft_shopee_nhom02.dto.CartDetailDTO;
 import com.example.fsoft_shopee_nhom02.dto.CartProductDTO;
 import com.example.fsoft_shopee_nhom02.model.CartProductEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Service
 public interface CartProductService {
-        List<CartProductEntity> addCartbyCartIdAndProductId(long productId, long userId, long qty) throws Exception;
+    List<CartProductEntity> addCartbyCartIdAndProductId(long productId, long userId, long qty) throws Exception;
 
-         void update(CartProductDTO cartProductDTO) ;
+    void update(CartProductDTO cartProductDTO);
 
-         void delete(long productId , long cartId);
+    void delete(long productId, long cartId);
+
+    List<CartDetailDTO> getAllCart(long cartId);
+    //List<HashMap<Object,Object>> getAllCart(long cartId);
+
     void updateQtyByCartId(long cartId, int qty, double price) throws Exception;
-        List<CartProductEntity> getCartByUserId(long cartId);
-        List<CartProductEntity> removeCartByUserId(long cartProductId,long cartId);
-        List<CartProductEntity> removeAllCartByUserId(long cartId);
-        Boolean checkTotalAmountAgainstCart(double totalAmount,long cartId);
-        List<CartProductEntity> getAllCheckoutByUserId(long cartId);
+
+    List<CartProductEntity> getCartByUserId(long cartId);
+
+    List<CartProductEntity> removeCartByUserId(long cartProductId, long cartId);
+
+    List<CartProductEntity> removeAllCartByUserId(long cartId);
+
+    Boolean checkTotalAmountAgainstCart(double totalAmount, long cartId);
+
+    List<CartProductEntity> getAllCheckoutByUserId(long cartId);
+
 
     // List<CartProductDTO> saveProductsForCheckout(List<CheckoutCart> tmp)  throws Exception;
 
-    }
+}
 
--- FShop/src/main/java/com/example/fsoft_shopee_nhom02/service/impl/CartProductServiceImpl.java
+++ FShop/src/main/java/com/example/fsoft_shopee_nhom02/service/impl/CartProductServiceImpl.java
@@ -1,20 +1,27 @@
 package com.example.fsoft_shopee_nhom02.service.impl;
 
 
+import com.example.fsoft_shopee_nhom02.dto.CartDetailDTO;
 import com.example.fsoft_shopee_nhom02.dto.CartProductDTO;
 import com.example.fsoft_shopee_nhom02.exception.NotFoundException;
 import com.example.fsoft_shopee_nhom02.model.CartEntity;
 import com.example.fsoft_shopee_nhom02.model.CartProductEntity;
 import com.example.fsoft_shopee_nhom02.model.ProductEntity;
+import com.example.fsoft_shopee_nhom02.model.TypeEntity;
 import com.example.fsoft_shopee_nhom02.repository.CartProductRepository;
 import com.example.fsoft_shopee_nhom02.repository.CartRepository;
+import com.example.fsoft_shopee_nhom02.repository.ProductRepository;
+import com.example.fsoft_shopee_nhom02.repository.TypeRepository;
 import com.example.fsoft_shopee_nhom02.service.CartProductService;
 import com.example.fsoft_shopee_nhom02.service.CartService;
 import com.example.fsoft_shopee_nhom02.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CartProductServiceImpl implements CartProductService {
@@ -26,26 +33,32 @@ public class CartProductServiceImpl implements CartProductService {
     CartRepository cartRepository;
 
     @Autowired
+    TypeRepository typeRepository;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
     ProductService proServices;
     @Autowired
     CartService cartService;
 
 
     @Override
-    public List<CartProductEntity> addCartbyCartIdAndProductId(long productId, long userId, long qty) throws Exception {
+    public List<CartProductEntity> addCartbyCartIdAndProductId(long productId, long cartId, long qty) throws Exception {
         try {
-            if(cartProductRepository.getCartByProductIdAnduserId(userId, productId).isPresent()){
+            if(cartProductRepository.getCartByProductIdAnduserId(cartId, productId).isPresent()){
                 throw new Exception("Product is already exist.");
             }
             CartProductEntity obj = new CartProductEntity();
             obj.setQuantity(qty);
-            CartEntity cart = cartService.getCartsById(userId);
+            CartEntity cart = cartService.getCartsById(cartId);
             obj.setCartEntity(cart);
             ProductEntity pro = proServices.getProductsById(productId);
             obj.setProductEntity(pro);
             //TODO price has to check with qty
             cartProductRepository.save(obj);
-            return this.getCartByUserId(userId);
+            return this.getCartByUserId(cartId);
         }catch(Exception e) {
             e.printStackTrace();
             throw new Exception(e.getMessage());
@@ -66,6 +79,38 @@ public class CartProductServiceImpl implements CartProductService {
     public void delete(long productId, long cartId) {
         cartProductRepository.deleteProduct(productId,cartId);
     }
+
+//    @Override
+//    public List<HashMap<Object, Object>> getAllCart(long cartId) {
+//        return cartProductRepository.getCartDetail(cartId);
+//    }
+
+      @Override
+        public List<CartDetailDTO> getAllCart(long cartId) {
+            List<CartProductEntity> cartProductEntities = cartProductRepository.getCart(cartId);
+            List<CartDetailDTO> cartDetailDTOS = new ArrayList<>();
+            for (CartProductEntity cartProduct : cartProductEntities){
+                CartDetailDTO cartDetailDTO = new CartDetailDTO();
+
+                cartDetailDTO.setQuantity(cartProduct.getQuantity());
+
+                ProductEntity product = productRepository.findById(cartProduct.getProductEntity().getId())
+                        .orElseThrow(()-> new NotFoundException("not found product"));
+                //set name and image
+                cartDetailDTO.setName(product.getName());
+                cartDetailDTO.setImage(product.getImageProduct());
+
+                //get type
+                TypeEntity type= typeRepository.findByType(cartProduct.getType());
+                cartDetailDTO.setType(cartProduct.getType());
+                cartDetailDTO.setPrice(type.getPrice());
+
+                cartDetailDTO.setTotalPrice(cartDetailDTO.getTotalPrice());
+                cartDetailDTOS.add(cartDetailDTO);
+            }
+            return cartDetailDTOS;
+        }
+
     @Override
     public void updateQtyByCartId(long cartId, int qty, double price) throws Exception {
     }
