a367dfd7f54dbefb76f235029e269d7a21635a80
Mai Thanh Nhã <nhamt0306@gmail.com>
2022-08-02 13:33:12 +0700

    add view and product sold

---
+++ FShop/src/main/java/com/example/fsoft_shopee_nhom02/dto/DayStatisticDTO.java
@@ -0,0 +1,40 @@
+package com.example.fsoft_shopee_nhom02.dto;
+
+public class DayStatisticDTO {
+    private String day;
+    private String month;
+    private String year;
+
+    public DayStatisticDTO() {
+    }
+
+    public DayStatisticDTO(String day, String month, String year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
