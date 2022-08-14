ae67791b803ec226995b412c0a1566d451983e47
VoHuuGiau2901 <vohuugiau2901@gmail.com>
2022-08-14 09:47:06 +0700

    fix notification bug

--- FShop/src/main/java/com/example/fsoft_shopee_nhom02/Notification/NotificationVar.java
+++
@@ -1,8 +0,0 @@
-package com.example.fsoft_shopee_nhom02.Notification;
-
-public class NotificationVar {
-    public static String destination = "/client";
-    public static String endpoint = "/NotificationService";
-    public static String sendPrefix = "/message";
-    public static String pushPrefix = "/user";
-}
