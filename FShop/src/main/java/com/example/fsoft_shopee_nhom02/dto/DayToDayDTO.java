8f3a1d263722224fa8f27fd3ace5ee13afe386bf
Mai Thanh Nhã <nhamt0306@gmail.com>
2022-08-26 22:31:53 +0700

    update file dto

--- FShop/src/main/java/com/example/fsoft_shopee_nhom02/dto/DayToDayDTO.java
+++ FShop/src/main/java/com/example/fsoft_shopee_nhom02/dto/DayToDayDTO.java
@@ -1,70 +1,24 @@
 package com.example.fsoft_shopee_nhom02.dto;
 
 public class DayToDayDTO {
-    private String day;
-    private String month;
-    private String year;
-    private String dayE;
-    private String monthE;
-    private String yearE;
-
+    private String daystart;
+    private String dayend;
     public DayToDayDTO() {
     }
 
-    public DayToDayDTO(String day, String month, String year, String dayE, String monthE, String yearE) {
-        this.day = day;
-        this.month = month;
-        this.year = year;
-        this.dayE = dayE;
-        this.monthE = monthE;
-        this.yearE = yearE;
+    public String getDaystart() {
+        return daystart;
     }
 
-    public String getDay() {
-        return day;
+    public void setDaystart(String daystart) {
+        this.daystart = daystart;
     }
 
-    public void setDay(String day) {
-        this.day = day;
+    public String getDayend() {
+        return dayend;
     }
 
-    public String getMonth() {
-        return month;
-    }
-
-    public void setMonth(String month) {
-        this.month = month;
-    }
-
-    public String getYear() {
-        return year;
-    }
-
-    public void setYear(String year) {
-        this.year = year;
-    }
-
-    public String getDayE() {
-        return dayE;
-    }
-
-    public void setDayE(String dayE) {
-        this.dayE = dayE;
-    }
-
-    public String getMonthE() {
-        return monthE;
-    }
-
-    public void setMonthE(String monthE) {
-        this.monthE = monthE;
-    }
-
-    public String getYearE() {
-        return yearE;
-    }
-
-    public void setYearE(String yearE) {
-        this.yearE = yearE;
+    public void setDayend(String dayend) {
+        this.dayend = dayend;
     }
 }
